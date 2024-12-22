744bdb8a9505e329d47dc260f8689e44351eefc8
yaoqiang-001 <yq2306387316@qq.com>
2024-12-22 16:30:06 +0800

    Update NumberFeature.java

--- src/main/java/org/example/NumberFeature.java
+++ src/main/java/org/example/NumberFeature.java
@@ -3,13 +3,31 @@ package org.example;
 import java.util.Scanner;
 
 public class NumberFeature {
-    public static void main(String[] args) {
-        int inputNumber; // 输入的数字
-        int result = 0; // 输出结果
-        Scanner in = new Scanner(System.in);
-        inputNumber = in.nextInt();
-        // 你的代码逻辑
+public static void main(String[] args) {
+int inputNumber; // 输入的数字
+int result = 0; // 输出结果
+Scanner in = new Scanner(System.in);
+inputNumber = in.nextInt();
+// 你的代码逻辑
 
-        System.out.println(result);
-    }
+digital_eigenvalue(inputNumber, result);
+
+
 }
+
+public static void digital_eigenvalue(int inputNumber, int result) {
+int number;
+int digit_position = 1;
+int binary_bit_value = 1;
+while (inputNumber > 0) {
+number = inputNumber % 10;
+inputNumber = inputNumber / 10;
+if (number % 2 == digit_position % 2)
+result += binary_bit_value;
+binary_bit_value *= 2;
+digit_position++;
+}
+System.out.println(result);
+}
+}
+
